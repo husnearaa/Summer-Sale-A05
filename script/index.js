c1550523fa752da9c60fc42ef40b5aeeefac754a
husneara <husnearaa99@gmail.com>
2023-08-21 19:22:07 +0600

    has compleled price, total price function and enable and disable part

---
+++ script/index.js
@@ -0,0 +1,52 @@
+let totalPrice = 0;
+function handleDivClick(target){
+  
+   const selectedItemsContainer = document.getElementById("selected-items")
+   const itemName = target.parentNode.childNodes[5].innerText;
+   const li = document.createElement('li');
+   li.innerText = itemName;
+   selectedItemsContainer.appendChild(li);
+
+   const price = target.parentNode.childNodes[6].innerText.split(" ")[0];
+   totalPrice = parseInt(totalPrice) + parseInt(price);
+   document.getElementById("total-price").innerText= totalPrice;
+
+
+   if(totalPrice >= 200){
+      let applyBtn = document.getElementById('apply-btn');
+
+      applyBtn.disabled = false;
+      applyBtn.style.backgroundColor = '#E527B2';
+   }
+
+
+   if(totalPrice > 0){  
+      let purchaseBtn = document.getElementById('purchase-btn');
+
+      purchaseBtn.disabled = false;
+      purchaseBtn.style.backgroundColor = '#E527B2';
+   }
+
+  
+     total = totalPrice;
+     document.getElementById("total").innerText= total;
+
+ 
+}
+
+// //   discount = (totalPrice*20)/100;
+// //   total = totalPrice - discount;
+  
+//  // total = totalPrice;
+//    // document.getElementById("total").innerText= total;
+
+
+// function discountBtnClick(){
+//     const couponField = document.getElementById('input-cp');
+//     const couponInput = couponField.value; 
+//     const getCouponCode = couponInput;
+   
+//    }
+
+ 
+
